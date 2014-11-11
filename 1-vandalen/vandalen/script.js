442e51ead5af2e3799c87c9a8154efd26b59d4ab
mi222ep1 <ingman.m@gmail.com>
2014-11-11 15:37:03 +0000

    borjar pa vandalen

--- 1-vandalen/vandalen/script.js
+++ 1-vandalen/vandalen/script.js
@@ -2,8 +2,28 @@
 
 var makePerson = function(persArr){
 
+ var result = {};
 
+var name = persArr.map(function(name) {return name + ', ';});
+//Map "name:" ', ' "name" ', ';
+//Map "age:"
+
+//minAge - nummer innehållande den ålder i de inskickade objekten som är lägst.
+//Sortera åldern, minAge = personArr[0] 
+//maxAge -nummer innehållande den ålder i de inskickade objekten som är högst.
+
+//avarageAge - nummer innehållande medelåldern av de inskickade objektens ålder.
+//Reduce allihopa, dela på antal delar
+
+//names - sträng innehållande samtliga personers namn separerade med ", " (komma och efterföljande mellanslag). Namnen sorteras i bokstavsordning.
 	// Din kod här...
 
+return result;
 }
+
+var data = [{name: "John Häggerud", age: 37}, {name: "Johan Leitet", age: 36}, {name: "Mats Loock", age: 46}];
+
+var result = makePerson(data);
+
+console.log(result);
 
