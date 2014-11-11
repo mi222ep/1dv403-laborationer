a4f5a9a209f92e95d1e2ec2ec40cf9ab51d2705e
mi222ep1 <ingman.m@gmail.com>
2014-11-11 17:25:07 +0000

    vandalen funkar som sig bor

--- 1-vandalen/vandalen/script.js
+++ 1-vandalen/vandalen/script.js
@@ -1,40 +1,53 @@
 "use strict";
 
-var makePerson = function(persArr){
+var makePerson = function(persArr) {
 
- var result = {};
+    var result = {};
 
-var names = persArr.map(function(persArr){return persArr.name + ', ';});
-var ages = persArr.map(function(persArr){return persArr.age;})
+    var names = persArr.map(function(persArr) {
+        return persArr.name;
+    });
+    names.sort(function(a,b){return a.localeCompare(b)});;
+    result.names = names.join(', ');
 
-ages.sort();
+    var ages = persArr.map(function(persArr) {
+        return persArr.age;
+    })
+    ages.sort();
 
-result.names = names.sort();
- 
+    result.minAge = ages[0];
+    result.maxAge = ages[persArr.length - 1];
+    result.averageAge = Math.round((ages.reduce(function(a, b) {
+        return a + b;
+    })) / (persArr.length));
 
-result.minAge = ages[0];
-result.maxAge = ages[persArr.length - 1];
-result.averageAge = Math.round((ages.reduce(function(a, b) {return a + b;}))/(persArr.length));
+    //Map "name:" ', ' "name" ', ';
+    //Map "age:"
 
-//Map "name:" ', ' "name" ', ';
-//Map "age:"
+    //minAge - nummer innehållande den ålder i de inskickade objekten som är lägst.
+    //Sortera åldern, minAge = personArr[0] 
+    //maxAge -nummer innehållande den ålder i de inskickade objekten som är högst.
 
-//minAge - nummer innehållande den ålder i de inskickade objekten som är lägst.
-//Sortera åldern, minAge = personArr[0] 
-//maxAge -nummer innehållande den ålder i de inskickade objekten som är högst.
+    //avarageAge - nummer innehållande medelåldern av de inskickade objektens ålder.
+    //Reduce allihopa, dela på antal delar
 
-//avarageAge - nummer innehållande medelåldern av de inskickade objektens ålder.
-//Reduce allihopa, dela på antal delar
+    //names - sträng innehållande samtliga personers namn separerade med ", " (komma och efterföljande mellanslag). Namnen sorteras i bokstavsordning.
+    // Din kod här...
 
-//names - sträng innehållande samtliga personers namn separerade med ", " (komma och efterföljande mellanslag). Namnen sorteras i bokstavsordning.
-	// Din kod här...
-
-return result;
+    return result;
 }
 
-var data = [{name: "John Häggerud", age: 37}, {name: "Johan Leitet", age: 36}, {name: "Mats Loock", age: 46}];
+var data = [{
+    name: "John Häggerud",
+    age: 37
+}, {
+    name: "Johan Leitet",
+    age: 36
+}, {
+    name: "Mats Loock",
+    age: 46
+}];
 
 var result = makePerson(data);
 
 console.log(result);
-
