fc05c219d45f825b67b7f615cde07772b8678980
mi222ep1 <ingman.m@gmail.com>
2014-11-22 14:25:24 +0000

    moment 3 done

--- 2-labbymezzage/messageboard.js
+++ 2-labbymezzage/messageboard.js
@@ -1,9 +1,20 @@
 var messageApp ={ 
+    
+    messages: [],
 
 init: function(){
-    var add = new Message("hej \n \n", new Date());
-    var axel = add.getHTMLText();
-    alert (axel);
+    
+    var test = new Message("test 1", new Date());
+    messageApp.messages.push(test);
+    
+    var test2 = new Message("test 2", new Date());
+    messageApp.messages.push(test2);
+    
+    var test3 = new Message("test 3", new Date());
+    messageApp.messages.push(test3);
+    
+    var hej = messageApp.messages[1].getText();
+    alert(hej);
 }
 };
 
