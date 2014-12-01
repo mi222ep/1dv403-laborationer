5c6d8469a8a8531c59555a3f23cc0945d3406eb9
mi222ep1 <ingman.m@gmail.com>
2014-12-01 18:01:13 +0000

    RenderTable works

--- 3-gameon/memory/script/memory.js
+++ 3-gameon/memory/script/memory.js
@@ -7,10 +7,33 @@ var memory ={
     init: function(){
         
         
-        var hello = new RandomGenerator.getPictureArray (3, 2);
+        memory.memoryArr = RandomGenerator.getPictureArray (3, 2);
         
-        alert(hello);
+        memory.RenderTable(3, 2);
+        alert(memory.memoryArr);
     },
+    RenderTable: function(rows, cols){
+        
+        var count = 1;
+        var table = document.createElement("table");
+        
+        for (var i = 0; i < rows; i++){
+            var tr = document.createElement("tr");
+            table.appendChild(tr);
+            
+            for (var j = 0; j < cols; j++){
+            var td =  document.createElement("td");
+            var cellText = document.createTextNode("Cell nummer "+count);
+            td.appendChild(cellText);
+            tr.appendChild(td);
+            count+=1;
+            }
+        }
+        
+        var memorySpace = document.querySelector("#memorySpace");
+        memorySpace.appendChild(table);
+    },
+    
     
     
     
