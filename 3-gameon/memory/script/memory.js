42e33d4c75b1d267397f1ac1735296d5026d7b15
mi222ep1 <ingman.m@gmail.com>
2014-12-01 20:03:04 +0000

    god natt

--- 3-gameon/memory/script/memory.js
+++ 3-gameon/memory/script/memory.js
@@ -6,15 +6,19 @@ var memory ={
     
     init: function(){
         
+        var row = 4;
+        var column = 4;
+
+    
+        memory.memoryArr = RandomGenerator.getPictureArray (row, column);
+        memory.RenderTable(row, column);
         
-        memory.memoryArr = RandomGenerator.getPictureArray (3, 2);
         
-        memory.RenderTable(3, 2);
-        alert(memory.memoryArr);
+        
     },
     RenderTable: function(rows, cols){
         
-        var count = 1;
+        var count = 0;
         var table = document.createElement("table");
         
         for (var i = 0; i < rows; i++){
@@ -23,20 +27,34 @@ var memory ={
             
             for (var j = 0; j < cols; j++){
             var td =  document.createElement("td");
-            var cellText = document.createTextNode("Cell nummer "+count);
-            td.appendChild(cellText);
+            var a = document.createElement("a");
+            var img = document.createElement("img");
+            img.setAttribute("src", "pics/"+memory.memoryArr[count]+".png");
+            a.setAttribute("href", "#");
+            a.appendChild(img);
+            
+            td.appendChild(a);
             tr.appendChild(td);
             count+=1;
-            }
+            a.onclick = function() {
+                memory.TurnTile();
+            };
+            
+                }
         }
         
         var memorySpace = document.querySelector("#memorySpace");
         memorySpace.appendChild(table);
+        
+        var memoryTiles = document.querySelectorAll("a");
+        memoryTiles.onclick = function() {
+            memory.TurnTile();
+        };
     },
-    
-    
-    
-    
+    TurnTile: function(){
+        alert("hejhej");
+    }
+
 };
 
 window.onload  = memory.init;
