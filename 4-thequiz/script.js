6b6f9b8f964007de95071592f762541f51e2615a
mi222ep1 <ingman.m@gmail.com>
2014-12-16 17:20:50 +0000

    comments added

--- 4-thequiz/script.js
+++ 4-thequiz/script.js
@@ -1,15 +1,15 @@
-"use strict"
+"use strict";
 
 window.onload = function(){
-    document.getElementById("knapp").addEventListener("click", function(){
+    
         var xhr = new XMLHttpRequest();
-        
+        var nextURL;
         xhr.onreadystatechange = function(){
             if(xhr.readyState === 4 && xhr.status === 200){
                 console.log(xhr.responseText);
                 var pers = JSON.parse(xhr.responseText);
                 console.log(pers.question);
-                
+                nextURL = pers.nextURL;
                 var div = document.getElementById("question");
                 div.innerHTML = pers.question;
             }
@@ -18,6 +18,24 @@ window.onload = function(){
         xhr.send(null);
         
         
+    document.getElementById("knapp").addEventListener("click", function(){
+     
+     var answer = new XMLHttpRequest();
+     
+     answer.onreadystatechange = function(){
+       if(answer.readyState === 4 && answer.status === 200){
+           console.log(answer.responseText);
+           
+       } 
+       answer.open("POST", nextURL, true);
+       answer.setRequestHeader("Content-Type", "application/json");
+       
+       //NYTT ANROP FÖR VARJE NY POST/GET? 
+       //Jag verkar inte få någon kontakt med servern nu, vad gör jag för fel?
+       
+       answer.send("EXAKT HUR SKALL SVARET SE UT?!");
+       
+     };
         
     });
     
