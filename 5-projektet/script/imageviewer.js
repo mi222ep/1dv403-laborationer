a0e816c53fbf0e85b0350d2aac755f2bd3f6917e
mi222ep1 <ingman.m@gmail.com>
2015-01-09 20:05:29 +0000

    width and length of thumbnail

--- 5-projektet/script/imageviewer.js
+++ 5-projektet/script/imageviewer.js
@@ -1,4 +1,9 @@
-window.onload = function(){
+var width = 0;
+var height = 0;
+
+function renderImageViewer(){
+    
+    
     
     var image = new XMLHttpRequest();
     
@@ -10,11 +15,24 @@ window.onload = function(){
          var arrLength  = imageInfo.length;
          for(var i = 0; i< arrLength; i+=1){
              console.log(imageInfo[i].URL);
+             getMaxWidthAndLength(imageInfo[i].thumbWidth, imageInfo[i].thumbHeight);
          }
+         console.log(width);
+         console.log(height);
         }
     };
     
     image.open("GET", "http://homepage.lnu.se/staff/tstjo/labbyServer/imgviewer/", true);
     image.send(null);
     
-};
+}
+function getMaxWidthAndLength(picWidth, picHeight){
+    if(picWidth>width){
+        width = picWidth;
+    }
+    if(picHeight>height){
+        height = picHeight;
+    }
+    
+}
+window.onload = renderImageViewer();
